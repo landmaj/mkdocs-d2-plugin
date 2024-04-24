901d5ba9f00965b6c482359bf1a1fe62285294b2
Michał Wieluński <michal@wielunski.net>
2024-04-24 14:16:32 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = with pkgs; mkShell {
-          packages = with pkgs; 
-          let
-            devpython = python3.withPackages
-              (packages: with packages; [ 
-                # PYTHON PACKAGES
-                virtualenv
-                pip
-                setuptools
-                wheel
-                twine
-              ]);
-          in [
-            # SYSTEM PACKAGES
-            devpython 
-            d2
-          ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs;
+            let
+              # CHANGE PYTHON VERSION
+              devpython = python3.withPackages
+                (packages: with packages; [
+                  # PYTHON PACKAGES
+                  virtualenv
+                  pip
+                  setuptools
+                  wheel
+                ]);
+            in
+            [
+              # SYSTEM PACKAGES
+              devpython
+            ];
 
           shellHook = ''
             # Setup the virtual environment if it doesn't already exist.
@@ -36,6 +43,6 @@
             fi
           '';
         };
-      }
-    );
+      });
+    };
 }
