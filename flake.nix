06caba0b35dc6b5d63e588e7241b41af416e1149
Michał Wieluński <michal@wielunski.net>
2026-04-09 21:33:10 +0200

    bump minimum Python version to 3.10

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs =
@@ -19,24 +19,29 @@
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-              python39
+              python310
               ruff
               uv
+              pre-commit
+              nil
             ];
 
           shellHook = ''
-            VENV=.venv
+            VENV_PATH=.venv
+            NEEDS_VENV=0
 
-            if test -d $VENV; then
+            if test ! -d $VENV_PATH; then
+              NEEDS_VENV=1
+            else
               NIX_PYTHON=$(python3 --version)
-              VENV_PYTHON=$(.venv/bin/python3 --version)
+              VENV_PYTHON=$(.venv/bin/python3 --version 2>/dev/null || echo "")
               if [ "$NIX_PYTHON" != "$VENV_PYTHON" ]; then
-                rm -rf $VENV
+                NEEDS_VENV=1
               fi
             fi
 
-            if test ! -d $VENV; then
-              uv venv --offline --python $(which python3) $VENV
+            if [ "$NEEDS_VENV" = "1" ]; then
+              uv venv --clear --offline --python $(which python3) $VENV_PATH
             fi
           '';
         };
